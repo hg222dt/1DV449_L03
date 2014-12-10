33569054d719314105f3bcc648108039225feabc
hg222dt <henrik.gudmundsson@gmail.com>
2014-12-10 17:38:56 +0100

    Fixat med info-rutor

--- HTMLView.php
+++ HTMLView.php
@@ -34,57 +34,11 @@ class HTMLView {
 				</div>
 				<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
                 <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
-                <!-- <script src='TrafficInfoBoard.js'></script> -->
+				<script src='js/TrafficInfoBoard.js'></script>
 
                 <script type='text/javascript'src='https://maps.googleapis.com/maps/api/js?key=AIzaSyC9HmJK7saacxO515M74v21H5PaDHXTNQQ'>
 				</script>
 				
-				<script type='text/javascript'>
-			      function initialize() {
-
-			      	var myLatlng = new google.maps.LatLng(62.88722932,17.91876062);
-
-			        var mapOptions = {
-			          center: myLatlng,
-			          zoom: 5
-			        };
-			        var map = new google.maps.Map(document.getElementById('map-canvas'),
-			            mapOptions);
-
-					var contentString = 'yoo';
-
-					  var infowindow = new google.maps.InfoWindow({
-					      content: contentString
-					  });
-
-					var marker = new google.maps.Marker({
-					    position: myLatlng,
-					    map: map,
-					    title:'Hello World!',
-						draggable:true,
-					    animation: google.maps.Animation.DROP
-					});
-					
-					google.maps.event.addListener(marker, 'click', function() {
-						infowindow.open(map,marker);
-    				});
-
-					google.maps.event.addListener(marker, 'click', toggleBounce);
-
-			      }
-
-			      function toggleBounce() {
-
-					  if (marker.getAnimation() != null) {
-					    marker.setAnimation(null);
-					  } else {
-					    marker.setAnimation(google.maps.Animation.BOUNCE);
-					  }
-					}
-
-			      google.maps.event.addDomListener(window, 'load', initialize);
-			    </script>
-
 			</body>
 			</html>
 		";
--- js/TrafficInfoBoard.js
+++ js/TrafficInfoBoard.js
@@ -1,4 +1,89 @@
-function init() {
+"use strict";
+
+var TRIN = TRIN || {};
+
+
+	TRIN.initialize = function() {
+
+			var myLatlng = new google.maps.LatLng(62.88722932,17.91876062);
+
+			var myLatlng2 = new google.maps.LatLng(62.88722932,19.91876062);
+
+		    var mapOptions = {
+		      center: myLatlng,
+		      zoom: 5
+		    };
+
+		    var map = new google.maps.Map(document.getElementById('map-canvas'),
+		        mapOptions);
+
+		    TRIN.addMarker(map, myLatlng);
+
+
+
+	}
+
+
+	TRIN.addMarker = function(map, myLatlng) {
+		var contentString = '<div id="content">'+
+							      '<div id="siteNotice">'+
+							      '</div>'+
+							      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+							      '<div id="bodyContent">'+
+							      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+							      'sandstone rock formation in the southern part of the '+
+							      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+							      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+							      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+							      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+							      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+							      'Aboriginal people of the area. It has many springs, waterholes, '+
+							      'rock caves and ancient paintings. Uluru is listed as a World '+
+							      'Heritage Site.</p>'+
+							      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+							      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+							      '(last visited June 22, 2009).</p>'+
+							      '</div>'+
+							      '</div>';
+
+			  var infowindow = new google.maps.InfoWindow({
+			      content: contentString
+			  });
+
+			var marker = new google.maps.Marker({
+			    position: myLatlng,
+			    map: map,
+			    title:'Hello World!',
+				draggable:true,
+			    animation: google.maps.Animation.DROP
+			});
+			
+			google.maps.event.addListener(marker, 'click', function() {
+				infowindow.open(map,marker);
+			});
+
+			google.maps.event.addListener(marker, 'click', TRIN.toggleBounce); 
+	}
+
+	TRIN.toggleBounce = function() {
+
+	  if (marker.getAnimation() != null) {
+	    marker.setAnimation(null);
+	  } else {
+	    marker.setAnimation(google.maps.Animation.BOUNCE);
+	  }
+
+	}
+
+
+  
+
+
+
+
+
+
+/*function init() {
   var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
   var mapOptions = {
     zoom: 4,
@@ -46,7 +131,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-/*
+
 var TrafficInfoBoard = {
 
 	trafficItems: [],
@@ -184,4 +269,7 @@ var TrafficInfoBoard = {
 }
 
 */
-window.onload = TrafficInfoBoard.init;
+
+// google.maps.event.addDomListener(window, 'load', TRIN.initialize);
+
+window.onload = TRIN.initialize;
