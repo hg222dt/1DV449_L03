ee77402a236b112256d2633cc4a69ae1b32c18e5
hg222dt <henrik.gudmundsson@gmail.com>
2014-12-16 17:06:19 +0100

    fixat cacheproblem

--- model/srApiHandler.php
+++ model/srApiHandler.php
@@ -3,19 +3,17 @@
 
 class SrApiHandler {
 
-	private $useCacheAuto;
 	private $maxAmountOfResults;
 	private $url;
 	private $filename;
 	private $cacheTimeInMinutes;
-	private $tryCache = false;
+	private $tryCache = true;
 
 	public function __construct() {
-		$this->useCacheAuto = false;
 		$this->maxAmountOfResults = 100;
 		$this->url = "http://api.sr.se/api/v2/traffic/messages?format=json&pagination=false&size=" . $this->maxAmountOfResults;
 		$this->filename = "jsonFile.txt";
-		$this->cacheTimeInMinutes = 15;
+		$this->cacheTimeInMinutes = 1;
 	}
 
 
@@ -30,40 +28,46 @@ class SrApiHandler {
 				$timestamp = $decodedJson->timestamp;
 				$cacheTime = date('Y/m/d H:i:s', strtotime('- ' . $this->cacheTimeInMinutes . ' minutes'));
 
-				if($cacheTime < $timestamp && $this->useCacheAuto == false) {
-					return $this->curl_get_request($this->url);
+
+				if($cacheTime < $timestamp) {
+					return $this->retrieveData();
 				} else {
-					return $decodedJson->retrievedData;
+					//return $decodedJson->retrievedData;
+					return json_encode($decodedJson, JSON_PRETTY_PRINT);
 				}
 			}
 		} else {
-
-			$ret = $this->curl_get_request($this->url);
-
-			$ret = json_decode($ret, true);
-
-			$messages = $ret['messages'];
-
-			$reversed = array_reverse($messages);
-
-			$trimmedMessages = array();
-
-			for ($i=0; $i < $this->maxAmountOfResults; $i++) { 
-				$trimmedMessages[$i] = $reversed[$i];
-			}
-
-
-			$jsonData = array(
-        		'timestamp' => date('Y/m/d H:i:s'),
-        		'retrievedData' => $trimmedMessages
-        	);
-
-        	$jsonData = json_encode($jsonData, JSON_PRETTY_PRINT);
-
-	   		file_put_contents($this->filename, $jsonData);
-
-			return $jsonData;
+			return $this->retrieveData();
 		}
+	}
+
+	private function retrieveData() {
+
+		$ret = $this->curl_get_request($this->url);
+
+		$ret = json_decode($ret, true);
+
+		$messages = $ret['messages'];
+
+		$reversed = array_reverse($messages);
+
+		$trimmedMessages = array();
+
+		for ($i=0; $i < $this->maxAmountOfResults; $i++) { 
+			$trimmedMessages[$i] = $reversed[$i];
+		}
+
+
+		$jsonData = array(
+    		'timestamp' => date('Y/m/d H:i:s'),
+    		'retrievedData' => $trimmedMessages
+    	);
+
+    	$jsonData = json_encode($jsonData, JSON_PRETTY_PRINT);
+
+   		file_put_contents($this->filename, $jsonData);
+
+		return $jsonData;
 	}
 
 	private function curl_get_request($url) {
